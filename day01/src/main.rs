95f13fe08d2f0ddbaf3ebdd335154d19d9dbc998
Daniel Herszenhut <dhersz@gmail.com>
2021-07-12 10:24:57 -0300

    a slightly better day01 answer
    experimented a bit with some error handling stuff, returning a
    Result<(), E> from main and using ? when collecting into a
    Result<Vec, _>, but i like panicking better here.
    
    also learned how to break from nested loops using a labelled loop.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,16 +1,25 @@
 fn main() {
     let expenses = include_str!("input.txt")
-        .split('\n')
-        .map(|i| i.parse::<i32>().unwrap_or(0)) // adds 0 to the end 
-                                                // due to trailing '\n'
-        .collect::<Vec<i32>>();
+        .lines()
+        .map(|i| i.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
 
-    for i in expenses.iter() {
+    'outer: for i in expenses.iter() {
         for j in expenses.iter() {
+            if i == j {
+                continue;
+            }
+
             for k in expenses.iter() {
+                if i == k || j == k {
+                    continue;
+                }
+
                 if *i + *k + *j == 2020 {
-                    println!("{}-{}-{}", i, j, k);
-                    println!("{}", i * j * k);
+                    println!("({}, {}, {})", i, j, k);
+                    println!("product: {}", i * j * k);
+
+                    break 'outer;
                 }
             }
         }
